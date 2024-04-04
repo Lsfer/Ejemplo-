badd7d6a542950a5f66784a100a6db09c6347f4b
Lsfer <160753199+Lsfer@users.noreply.github.com>
2024-04-04 10:30:23 -0600

    Add files via upload

---
+++ primer ejercicio infinito.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int control = 1; 
+	
+	do {
+		cout << "hola estudiantes"; 
+		cout << endl;
+		cout << "vienvenidos al ciclo"; 
+		cout << "¿desea repetir? 1. Si , 2. NO"; 
+		cin >> control;
+		
+	} while(control = 1); 
+	cout << "salimos del ciclo"; 
+	return 0;
+}
+
