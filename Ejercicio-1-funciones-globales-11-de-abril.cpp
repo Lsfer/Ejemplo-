8d8aad22d89882f7cfadc6617bf9fc3e6ba62648
Lsfer <160753199+Lsfer@users.noreply.github.com>
2024-04-11 17:49:50 -0600

    Add files via upload

---
+++ Ejercicio-1-funciones-globales-11-de-abril.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+void Mensaje1() {
+	int opcion1; 
+	cout << "Hola usted esta en Ingenieria en sistemas."  << endl; 
+	
+}
+
+void Mesnaje2() {
+	int opcion2; 
+	cout << "Usted perdio progra 1. "  << endl;  
+}
+
+void Mensaje3() {
+	int opcion3; 
+	cout << "Hola mundo." << endl; 
+	
+}
+
+void Mensaje4() {
+	int opcion4; 
+	cout << "¿Un League of Legens?."  << endl; 
+	
+}
+
+void Mensaje5() {
+	int opcion5; 
+	cout << "salir del programa."  << endl; 
+	
+}
+
+int main(int argc, char *argv[]) {
+	int opcion;
+	
+	
+	do{
+		cout << "1. opcion1" << endl; 
+		cout << "2. opcion2" << endl; 
+		cout << "3. opcion3" << endl; 
+		cout << "4. opcion4" << endl; 
+		cout << "5. opcion5" << endl;
+		cout << "Hola elija una opcion del 1 al 5. " << endl; 
+		
+		cin >> opcion;
+			switch (opcion) {
+			case 1: 
+				Mensaje1(); 
+				break; 
+			case 2: 
+				Mesnaje2(); 
+				break; 
+			case 3: 
+				Mensaje3(); 
+				break; 
+			case 4: 
+				Mensaje4(); 
+				break; 
+			case 5: 
+				Mensaje5(); 
+				break; 
+			default: 
+				cout << "esta opcion no es valida :)" << endl; 
+				break; 
+		}
+	} while(opcion !=5);
+	return 0;
+}
+
---
+++ Ejercicio-2-funciones-globales-11-de-abril.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void pedirnumero() {
+	int numero;
+	cout << "ingrese un numero: " << endl;
+	cin >> numero; 
+	cout << "tu numero ingresado es: " << numero << endl; 
+}
+
+void pedirunapalabra() {
+	string palabra; 
+	cout << "ingrese una palabra: "  << endl; 
+	cin >> palabra; 
+	cout << "Tu palabra es: " << palabra << endl; 
+}
+void pedirunaletra() {
+	char letra; 
+	cout << "ingrese una letra: " << endl;
+	cin >> letra; 
+	cout << "tu letra es: " << letra << endl; 
+}
+int main(int argc, char *argv[]) {
+	char continuarmenu; 
+	int opcion; 
+	
+	do {
+		cout << "1. Numero: " << endl; 
+		cout << "2. Palabra: " << endl; 
+		cout << "3. Letra: " << endl; 
+		cout << "Por favor ingrese de las siguientes opciones (1 al 3): " << endl; 
+		cin >> opcion; 
+		
+		switch(opcion) {
+		case 1:
+			pedirnumero(); 
+			break; 
+		case 2: 
+			pedirunapalabra(); 
+			break; 
+		case 3: 
+			pedirunaletra(); 
+			break;
+		default: 
+			cout << "opcion no valida" << endl; 
+		}
+		cout << "¿Desea continuar con el menu? s/n: "; 
+		cin >> continuarmenu; 
+		
+	} while(continuarmenu == 's');
+	
+	cout << "fin" << endl; 
+	return 0;
+}
+
