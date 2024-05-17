6b12840443e1de8059cf7d8aa3f44c752f6a8e4c
Lsfer <160753199+Lsfer@users.noreply.github.com>
2024-05-16 20:13:30 -0600

    Add files via upload

---
+++ Tarea1-cifrado-16-de-mayo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int cifradodecontra(int cn, int num ) {
+	int sm = cn + num; 
+	int mult = sm * num; 
+	return mult;
+}
+
+
+int main(int argc, char *argv[]) {
+	int cn, num, contc; 
+	
+	cout << "ingrese una contraseña: " ; 
+	cin >> cn; 
+	
+	cout << "imgrese un numero: "; 
+	cin >> num; 
+	
+	contc = cifradodecontra(cn, num); 
+	cout << "su contrasseña finalizada es : " << contc << endl; 
+	return 0;
+}
+
---
+++ tarea2-cifrado-16-de-mayo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string frase(const string & cnt){
+	string respuesta = cnt; 
+	int size = respuesta.length(); 
+	for(int i = 0; i < size; i++) {
+		char caracter = respuesta[i];
+		if(caracter == 'i' || caracter == 'I') {
+			respuesta[i]= '1'; 
+		}
+		if(caracter == 'e' || caracter == 'E') {
+			respuesta[i]= '3'; 
+		}
+		if(caracter == 'a' || caracter == 'A') {
+			respuesta[i]= '4'; 
+		}
+		if(caracter == 'o' || caracter == 'O') {
+			respuesta[i]= '0'; 
+		}
+		if(caracter == 'u' || caracter == 'U') {
+			respuesta[i]= '*'; 
+		}
+	}
+	return respuesta;
+}
+int main(int argc, char *argv[]) {
+	string fr, frcifrada; 
+	
+	cout << "ingrese una frase para que se cifre: " ; 
+	
+	getline(cin,fr); 
+	
+	frcifrada =  frase(fr); 
+	cout << "frase cifrada: " << frcifrada <<endl; 
+	return 0;
+}
+
